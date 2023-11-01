6aa79ec43eea453b5851e535b25c0047d518dd9b
Nathan Rogan <rolorogan@gmail.com>
2023-11-01 10:17:43 +0000

    Initial commit

---
+++ src/assets/wmcads/README.md
@@ -0,0 +1,5 @@
+# assets/wmcads folder
+
+This folder contains various scss variables that are using in the wmca design system.
+
+This makes it easy to reference the correct colours and sizes when you need to do something custom within the app.
---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="wmcads-container wmcads-p-t-lg wmcads-p-b-lg wmcads-grid">
+      <div className="wmcads-col-1">
+        <h2>Hello West Midlands!</h2>
+        <p>Don&apos;t forget to:</p>
+        <ul>
+          <li>
+            Update the variables in <strong>.env</strong> file
+          </li>
+          <li>
+            Update the <strong>breadcrumbs</strong>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
